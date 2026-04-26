f1734b88d0c1b955176326b80d8d868ff6c6878f
Ayoub <ab.ouarain@gmail.com>
2026-04-26 19:29:15 +0100

    chore: add google analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "@/styles/globals.css";
 
 const inter = Inter({
@@ -69,6 +70,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-64KWD5VRJ8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-64KWD5VRJ8');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
